bb3e6094e624a3434334093e8eb2ef0142b5cd7d
Chihab <blackpang23@gmail.com>
2018-12-08 13:04:18 +0100

    Added Logo Added Themeing Added Sign up form Added sign up Java

---
+++ app/src/main/java/com/example/black/ragnarok2/UserActivity.java
@@ -0,0 +1,40 @@
+package com.example.black.ragnarok2;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class UserActivity extends AppCompatActivity {
+
+    private static final String TAG = "UserActivity";
+
+    private TextView greetingTextView;
+    private Button btnLogOut;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+        Bundle bundle = getIntent().getExtras();
+        String user = bundle.getString("username");
+        greetingTextView = findViewById(R.id.greeting_text_view);
+        btnLogOut = findViewById(R.id.logout_button);
+        greetingTextView.setText("Hello "+ user);
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
+
+
